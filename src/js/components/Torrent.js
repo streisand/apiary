4599c47e66541b20d3a50bdc53526e03b9535c34
uzarubin <ustin.zarubin@campusbellhops.com>
2015-04-01 18:44:09 -0400

    Removing public/ from cache

--- src/js/components/Torrent.js
+++ src/js/components/Torrent.js
@@ -12,15 +12,22 @@ var Torrent = React.createClass({
     return (
       <div className="torrent">
         <p className="torrent-info torrent-name">{this.props.name}</p>
-        <p className="torrent-info download-amount">{this.props.downloadAmount}</p>
-        <p className="torrent-info">{this.props.downloadSpeed}</p>
-        <p className="torrent-info">{this.props.uploadSpeed}</p>
-        <p className="torrent-info total-size">{this.props.totalSize}</p>
-        <p className="torrent-info progress-bar"><progress  value={this.props.downloadAmount} max={this.props.totalSize}></progress></p>
+        <p className="torrent-info download-amount">{this.bytesToSize(this.props.downloadAmount)}</p>
+        <p className="torrent-info">{this.bytesToSize(this.props.downloadSpeed)}/s</p>
+        <p className="torrent-info">{this.bytesToSize(this.props.uploadSpeed)}/s</p>
+        <p className="torrent-info total-size">{this.bytesToSize(this.props.totalSize)}</p>
+        <p className="torrent-info progress-bar"><progress value={this.props.downloadAmount} max={this.props.totalSize}></progress></p>
       </div>
     )
-  }
+  },
 
+  bytesToSize: function(bytes) {
+    if(bytes == 0) return '0 B';
+    var k = 1024 ;
+    var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toFixed(1) + ' ' + sizes[i];
+  }
 
 });
 
