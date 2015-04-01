d4f997afd64b54ef629e8e2e396f8f00aa0a3426
uzarubin <ustin.zarubin@campusbellhops.com>
2015-04-01 19:56:34 -0400

    Daniel is a dick

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var scgi = require("scgi-stream");
+var RPC  = require("xmlrpc-stream");
+
+var rpc = new RPC(function() {
+  return scgi.duplex({
+    host: "127.0.0.1",
+    port: 4000,
+    path: "/"
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
