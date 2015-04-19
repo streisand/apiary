36500d22cd9577d0f76de64c6468d77f50101f5b
uzarubin <ustin.zarubin@campusbellhops.com>
2015-04-18 21:16:31 -0400

    Changing method from "d.down_total" to "d.completed_bytes".

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,13 +1,6 @@
 var scgi = require("scgi-stream");
 var RPC  = require("xmlrpc-stream");
 
-var rpc = new RPC(function() {
-  return scgi.duplex({
-    host: "127.0.0.1",
-    port: 4000,
-    path: "/"
-  });
-});
 
 function SocketController(socket) {
   this.io = socket;
@@ -36,17 +29,33 @@ SocketController.prototype = {
   constructor: SocketController,
 
   fetchTorrents: function(socket) {
-    this.rpc.call("d.multicall", "default", "d.get_hash=", "d.get_name=",  "d.get_bytes_done=",  "d.get_down_rate=", "d.get_up_rate=", "d.get_creation_date=", "d.get_size_bytes=", function(err, res) {
+    var torrents = [];
+    this.rpc.call(
+      "d.multicall",
+      "default",
+      "d.get_hash=",
+      "d.get_name=",
+      "d.get_bytes_done=",
+      "d.get_down_rate=",
+      "d.get_completed_bytes=",
+      "d.get_up_rate=",
+      "d.get_up_total=",
+      "d.get_creation_date=",
+      "d.get_size_bytes=",
+      "or={d.get_up_rate=,d.get_down_rate=}", function(err, res) {
       torrents = res.map(function(torrentInfo) {
         var torrentObj = {};
         torrentObj.infoHash = torrentInfo[0];
         torrentObj.name = torrentInfo[1];
         torrentObj.downloadAmount = torrentInfo[2];
         torrentObj.downloadSpeed = torrentInfo[3];
-        torrentObj.uploadSpeed = torrentInfo[4];
-        torrentObj.creationDate = torrentInfo[5];
-        torrentObj.totalSize = torrentInfo[6];
-        torrentObj.isActive = torrentInfo[7] === 1;
+        torrentObj.downloadTotalSize = torrentInfo[4];
+        torrentObj.uploadSpeed = torrentInfo[5];
+        torrentObj.uploadTotalSize = torrentInfo[6];
+        torrentObj.creationDate = torrentInfo[7];
+        torrentObj.totalSize = torrentInfo[8];
+        torrentObj.isActive = torrentInfo[9] === 1;
+
         return torrentObj;
       });
       socket.emit('fetch-torrents', torrents);
