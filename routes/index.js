d8dd29645543081aeeefce7159816028a2b1dc6e
uzarubin <ustin.zarubin@campusbellhops.com>
2015-04-18 21:42:45 -0400

    Removing things that we no longer need.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var scgi = require("scgi-stream");
-var RPC  = require("xmlrpc-stream");
-
-var rpc = new RPC(function() {
-  return scgi.duplex({
-    host: "127.0.0.1",
-    port: 4000,
-    path: "/"
-  });
-});
 
 /* GET home page. */
 router.get('/', function(req, res) {
